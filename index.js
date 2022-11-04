604339b2acea3aacc25a43e95094053065d511d3
Mars Chng <wayne.chuang.27@gmail.com>
2022-11-04 20:47:50 +0800

    Vue ABCs: 用 filter() 做出搜尋功能

--- index.js
+++ index.js
@@ -1,9 +1,23 @@
 const Playlist = {
     data () {
         return {
-            playlist: ['暗流', '新世紀的女兒', 'I Hear a Symphony']
+            playlist: ['暗流', '新世紀的女兒', 'I Hear a Symphony'],
+            searchTerm: "",
         };
     },
+    computed: { // 相當於 js 的 .filter()
+        filteredPlaylist () {
+            if (this.searchTerm) {
+                return this.playlist.filter((song) => 
+                    song.toLowerCase().includes(this.searchTerm.toLowerCase())
+                    // 歌名包含搜尋內容則 return, 英文一綠 toLowerCase比對
+                );
+            } else {
+                return this.playlist;
+            }
+        }
+    }
 };
 
 Vue.createApp(Playlist).mount('#app');
+// createApp 以前叫 new Vue();
